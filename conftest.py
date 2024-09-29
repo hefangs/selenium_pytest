704176c23d929f1b83f6739405e328056c745026
何方 <he529564582@163.com>
2024-09-30 00:11:26 +0800

    feat:从类中剥离fixture到conftest

--- conftest.py
+++ conftest.py
@@ -1,13 +1,26 @@
+import datetime
 
 import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
 
+
+
+
+
+def pytest_configure(config):
+    current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    log_file = f"./logs/{current_time}.log"
+    config.option.log_file = log_file
+
+
 @pytest.fixture(scope="class")
-def setup(self):
-    self.driver = webdriver.Edge()
-    self.driver.get("https://music.163.com")
-    self.driver.maximize_window()
-    self.wait = WebDriverWait(self.driver, 10)
+def my_fix(request):
+    driver = webdriver.Edge()
+    driver.get("https://music.163.com")
+    driver.maximize_window()
+    wait = WebDriverWait(driver, 10)
+    request.cls.driver = driver  # 绑定到类
+    request.cls.wait = wait      # 绑定到类
     yield
-    self.driver.quit()
+    driver.quit()
